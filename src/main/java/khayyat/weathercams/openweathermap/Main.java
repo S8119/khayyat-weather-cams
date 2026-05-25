c6f4baf20a1648479a35b37da0b6c206cb4d9dab
Shulamit Khayyat <skhayyat@student.touro.edu>
2026-05-25 11:41:57 -0400

    Changed temps and feelsLike to Double so WeatherServiceTest can use assertNotNull to ensure they received a valid value.

--- src/main/java/khayyat/weathercams/openweathermap/Main.java
+++ src/main/java/khayyat/weathercams/openweathermap/Main.java
@@ -2,6 +2,6 @@ package khayyat.weathercams.openweathermap;
 
 import com.google.gson.annotations.SerializedName;
 
-public record Main(double temp, @SerializedName("feels_like") double feelsLike)
+public record Main(Double temp, @SerializedName("feels_like") Double feelsLike)
 {
 }
